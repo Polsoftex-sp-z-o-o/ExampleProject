3b3e8cdbaec0607d5a0e91fbc5774f49cfdbe580
Atloas <miter010@gmail.com>
2021-03-18 21:47:24 +0100

    Example Spring project

---
+++ ExampleProject/src/test/java/com/Polsoftex/ExampleProject/ExampleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Polsoftex.ExampleProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
